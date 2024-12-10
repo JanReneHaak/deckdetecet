3e3f0507ae1894d99dcc59bd0fa92f89fa020817
Jan Rene Haak <jan.rene.haak@gmail.com>
2024-12-10 18:16:48 +0100

    The card names get extracted successfully using TrOCR. Working on cleaning up the output string to match it with the database

--- get_card_name.py
+++ get_card_name.py
@@ -1,33 +1,52 @@
 import pandas as pd
 import numpy as np
-import pytesseract
 from preprocess import preprocessing
-import matplotlib.pyplot as plt
-import os
-from os import listdir
 from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+import string
+from fuzzywuzzy import process
 
-## Generating the card dataframe
+def get_card_name(image_path):
+    image = preprocessing(image_path, "name")
+    text = image_to_text(image)
+    name = clean_up(text)
+    return name
 
-processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-stage1")
-model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-base-stage1")
 
-
-def get_card_name(processor, model, image):
+def image_to_text(image):
+    processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-stage1")
+    model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-base-stage1")
     pixel_values = processor(images=image, return_tensors="pt").pixel_values
     generated_ids = model.generate(pixel_values)
     text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
     return text
 
 
-names = []
-folder_dir = "raw_data"
-for images in os.listdir(folder_dir):
-    # check if the image ends with png
-    if (images.endswith(".jpg")):
-        path = f"{folder_dir}/{images}"
-        image = preprocessing(path, "name")
-        tmp = get_card_name(processor, model, image)
-        names.append(tmp)
+def clean_up(text):
+    text = text.strip()
+    text_list = [letter for letter in text if not letter.isdigit()]
+    for k, v in enumerate(text_list):
+        if v in string.punctuation:
+            text_list.pop(k)
+    first_cleanup = "".join(text_list)
+    for char in string.punctuation:
+        name = first_cleanup.replace(char, "")
+    return name
 
-print(names)
+
+card_name = get_card_name("raw_data/images/test_15.jpg")
+# cards = pd.read_json("raw_data/2024-12-09_default-cards.json")
+# card_names = cards["name"].tolist()
+
+# # Perform fuzzy search
+# match, score = process.extractOne(card_name, card_names)
+
+# # Get the matching card
+# card = cards[cards["name"] == match]
+# print(f"Best Match: {match} (Score: {score})")
+# print(card["name"])
+
+for char in string.punctuation:
+    if char in card_name:
+        print("Ey")
+
+print(card_name)
